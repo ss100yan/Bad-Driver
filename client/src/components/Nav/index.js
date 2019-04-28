f608c4c5a3c239ffcf32aa7584c5ab193a49e854
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-28 17:50:08 -0400

    nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,17 +2,41 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        South Florida Bad Drivers
-        <br/>
-        <a className="navbar-brand" href="/">Login</a>
-        <a className="navbar-brand" href="/add">Add Bad Drivers</a>
-      </a>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    //   <a className="navbar-brand" href="/">
+    //     South Florida Bad Drivers
+    //     <br/>
+    //     <a className="navbar-brand" href="/">Login</a>
+    //     <a className="navbar-brand" href="/add">Add Bad Drivers</a>
+    //   </a>
+    //         </nav>
+
+
+    <nav class="navbar navbar-inverse navbar-dark bg-dark ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">SFL Bad drivers</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/">Home</a></li>
+      <li class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="/add">Add<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+        
+          {/* <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li> */}
+        </ul>
+      </li>
+      {/* <li><a href="#">Page 2</a></li> */}
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       
-       
-          
-    </nav>
+    </ul>
+  </div>
+</nav>
   );
 }
 
