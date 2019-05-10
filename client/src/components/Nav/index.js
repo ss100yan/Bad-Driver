27b7f333e7b000d88d73b1d99421c32f418b4db3
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-05-10 02:28:56 -0400

    new

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,20 +4,23 @@ import GoogleLogin from 'react-google-login';
 import { GoogleLogout } from 'react-google-login';
 
 
+
+
+
 function Nav() {
-  
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    {localStorage.clear()}
+    
+  }
 
   // state={
   //   hideLogin:false,
   //   hideLogout:true
   // };
-  // function ActionLink() {
-  //   function handleClick(e) {
-  //     e.preventDefault();
-  //     console.log('The link was clicked.');
-  //     localStorage.clear()
-  //   }
-
+ 
   return (
 
     <nav class="navbar navbar-inverse navbar-dark bg-dark ">
@@ -37,7 +40,7 @@ function Nav() {
       <li><a><span class="glyphicon glyphicon-user"></span>{localStorage.getItem("name")}</a></li>
       {/* <img src={localStorage.getItem("photo")} alt="image" style={{height:20 ,width: 20 }} />  */}
       {/* {localStorage.getItem("isLoggedIn")} */}
-      {/* <li style={{visibility: "visible"}}><a href="#"  ><span class="glyphicon glyphicon-log-out" ></span> Logout</a></li> */}
+      <li style={{visibility: "visible"}}><a href="#" onClick={handleClick}  ><span class="glyphicon glyphicon-log-out" ></span> Logout</a></li>
       <li style={{visibility: "visible" }}><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       
     </ul>
