60dbf9991095a033d86b2daf0e0d7d3ff12727ca
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-05-10 01:46:46 -0400

    new

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,6 +5,13 @@ import { GoogleLogout } from 'react-google-login';
 
 
 function Nav() {
+  
+
+  // state={
+  //   hideLogin:false,
+  //   hideLogout:true
+  // };
+
   return (
 
     <nav class="navbar navbar-inverse navbar-dark bg-dark ">
@@ -24,8 +31,8 @@ function Nav() {
       <li><a><span class="glyphicon glyphicon-user"></span>{localStorage.getItem("name")}</a></li>
       {/* <img src={localStorage.getItem("photo")} alt="image" style={{height:20 ,width: 20 }} />  */}
       {/* {localStorage.getItem("isLoggedIn")} */}
-      {/* <li style={{visibility: "visible"}}><a href="/logout"><span class="glyphicon glyphicon-log-out" ></span> Logout</a></li> */}
-      <li style={{visibility: "visible"}}><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li style={{visibility: "visible"}}><a href="#" onClick={localStorage.clear()} ><span class="glyphicon glyphicon-log-out" ></span> Logout</a></li>
+      <li style={{visibility: "visible" }}><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       
     </ul>
   </div>
