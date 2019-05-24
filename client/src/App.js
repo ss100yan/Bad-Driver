a5785739df5cd62a1739132bc37fcde7b7b90566
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-05-24 01:18:52 -0400

    clean extras

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/MainPage";
-import AddBadDriver from "./pages/AddBadDriver";
-import login from "./pages/login";
-import geoLocation from "./components/geoLocation";
-
-// import logout from "./pages/logout";
-import Detail from "./pages/Comment";
 import NoMatch from "./pages/NoMatch";
 import PersistentDrawerLeft from "./components/Nav";
 import './App.css';
@@ -19,14 +13,11 @@ function App() {
     <Router>
       <div className="bg">
         <PersistentDrawerLeft />
-        {/* <geoLocation/> */}
-        <Switch>
-          <Route exact path="/login" component={login} />
-          {/* <Route exact path="/logout" component={logout} /> */}
-          {/* <Route exact path="/add" component={AddBadDriver} /> */}
+          <Switch>
+            
           <Route exact path="/" component={Books} />
          
-          */}
+          
           <Route component={NoMatch} />
         </Switch>
       </div>
